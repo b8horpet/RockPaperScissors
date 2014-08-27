2869fa5d7763bbcc8f307f8f7f222e37faf84916
Horváth Péter <b8horpet@gmail.com>
2014-08-27 22:40:55 +0200

    Using boost version info

--- rps.cpp
+++ rps.cpp
@@ -3,6 +3,10 @@
 //#include <readline/readline.h>
 #include <ncurses.h>
 
+//#include <boost/asio.hpp>
+//#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/version.hpp>
+
 int main(int argc, char** argv)
 {
 	initscr();
@@ -10,6 +14,7 @@ int main(int argc, char** argv)
 	keypad(stdscr, TRUE);
 	noecho();
 	printw("Rock Paper Scissors Game\n");
+	printw("Built with Boost v %d.%02d.%02d\n",BOOST_VERSION/100000,(BOOST_VERSION/100)%100,BOOST_VERSION%100);
 	refresh();
 	getch();
 	printw("Exiting ");
