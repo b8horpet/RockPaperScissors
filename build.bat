acb2c9b9213e43137813c8bd381dcce34c757d96
Horváth Péter <b8horpet@gmail.com>
2014-09-01 02:50:48 +0200

    static linkage to create standalone exe

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 mkdir output
-g++ rps.cpp -g -o output/rps.exe -D_WIN32_WINNT=0x0601 -I C:/local/boost_1_56_0/ -L C:/local/boost_1_56_0/bin.v2/libs/system/build/gcc-mingw-4.8.1/release/link-static/threading-multi/ -lboost_system-mgw48-mt-1_56 -lws2_32 -lpdcurses
+g++ rps.cpp -g -o output/rps.exe -D_WIN32_WINNT=0x0601 -I C:/local/boost_1_56_0/ -L C:/local/boost_1_56_0/bin.v2/libs/system/build/gcc-mingw-4.8.1/release/link-static/threading-multi/ -lboost_system-mgw48-mt-1_56 -lws2_32 -static -lpdcurses -static-libgcc -static-libstdc++
