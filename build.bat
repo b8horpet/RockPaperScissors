43af3bec80f82277a8888459682606c3b0df644b
Horváth Péter <b8horpet@gmail.com>
2015-02-05 00:50:59 +0100

    added program options support for windows

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 mkdir output
-g++ rps.cpp -g -o output/rps.exe -D_WIN32_WINNT=0x0601 -I C:/local/boost_1_56_0/ -L C:/local/boost_1_56_0/bin.v2/libs/system/build/gcc-mingw-4.8.1/release/link-static/threading-multi/ -lboost_system-mgw48-mt-1_56 -lws2_32 -static -lpdcurses -static-libgcc -static-libstdc++
+g++ rps.cpp -g -o output/rps.exe -D_WIN32_WINNT=0x0601 -I C:/local/boost_1_56_0/ -L C:/local/boost_1_56_0/bin.v2/libs/system/build/gcc-mingw-4.8.1/release/link-static/threading-multi/ -lboost_system-mgw48-mt-1_56 -L C:/local/boost_1_56_0/bin.v2/libs/program_options/build/gcc-mingw-4.8.1/release/link-static/threading-multi/ -lboost_program_options-mgw48-mt-1_56 -lws2_32 -static -lpdcurses -static-libgcc -static-libstdc++
+
